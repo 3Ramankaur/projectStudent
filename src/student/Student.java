ddb762b6e38e0c926d7f87f0cf11a46a7917a95a
3RamanKaur <kau11821@sheridancollege.ca>
2022-05-19 11:37:16 -0400

    version 1 of student project

---
+++ src/student/Student.java
@@ -0,0 +1,21 @@
+
+
+package student;
+
+/**
+ *
+ * @author Ramandeep Kaur
+ */
+public class Student {
+
+     private String name;
+    private int age;
+
+ public Student(String name, int age){
+     this.name=name;
+     this.age=age;}
+ 
+ public String toString(){
+     return("Student name: "+ name + " age: "+age);}
+
+}
---
+++ src/student/TestStudent.java
@@ -0,0 +1,25 @@
+
+
+package student;
+
+/**
+ *
+ * @author Ramandeep Kaur
+ */
+public class TestStudent {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Student[] details=new Student[3];
+     details[0]=new Student("Raman", 21);
+     details[1]=new Student("Karan",20);
+     details[2]=new Student("Taniya",22);
+
+     for(int i=0;i<details.length;i++){
+         System.out.println(details[i].toString());}
+    }
+
+}
