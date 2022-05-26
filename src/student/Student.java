cf3dee7f372f8c0f25e5d64ab5ed434a17d48da0
3RamanKaur <kau11821@sheridancollege.ca>
2022-05-26 11:00:20 -0400

    version 2 branch of student project

--- src/student/Student.java
+++ src/student/Student.java
@@ -10,12 +10,27 @@ public class Student {
 
      private String name;
     private int age;
-
+    private boolean parttime;
+    
  public Student(String name, int age){
      this.name=name;
      this.age=age;}
  
  public String toString(){
      return("Student name: "+ name + " age: "+age);}
+
+    /**
+     * @return the parttime
+     */
+    public boolean isParttime() {
+        return parttime;
+    }
+
+    /**
+     * @param parttime the parttime to set
+     */
+    public void setParttime(boolean parttime) {
+        this.parttime = parttime;
+    }
 
 }
